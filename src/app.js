42b7e629509d19d72d4ec44814f7e4bf17ac0f81
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-02 17:22:34 +0600

    added comments on application
    added applications details on every steps

--- src/app.js
+++ src/app.js
@@ -1,30 +1,42 @@
+// Adding References on application
 const fs = require("fs");
 const steem = require('steem');
 const rlinq = require('linq');
 
-var preresteemed = [];
-
+// This will retrive data from config.json and store on config which will use on application runtime
 config = JSON.parse(fs.readFileSync("config.json"));
 
-
+// This array will stores userlist from config file
 var userlistforResteem = config.userlist;
 
+// This array will stores Previous Resteemed post  when application load
+var preresteemed = [];
+
+// This will Retrive the Data of resteemed.json file
 if (fs.existsSync('resteemed.json')) {
     const resteemed = JSON.parse(fs.readFileSync("resteemed.json"));
     if (resteemed.length) {
+        // Storing resteemed Data to preresteemed which will use on application runtime
         preresteemed = resteemed;
     }
 }
 
+// This will show notification when using
 console.log('Application Initialized...');
-//setInterval(resteemPost, 300000);
+
+//Calling function to Resteem Post
 resteemPost();
 
 function resteemPost() {
+    //Storing your username from config
     var username = config.sender_account;
+
+    //Storing your private_posting_key from config
     var wif = config.private_posting_key;
 
+    //Creating a loop on userlist
     for (var i = 0; i < userlistforResteem.length; i++) {
+        // This function will retrive the post of selected users, limit parameter defines how many post you want to retrive
         steem.api.getDiscussionsByBlog({ tag: userlistforResteem[i], limit: 1 }, function (err, result) {
             if (err === null) {
                 if (result.length > 0 && IsValidPost(result[0].author, result[0].permlink)) {
@@ -36,9 +48,11 @@ function resteemPost() {
                     steem.broadcast.customJson(wif, [], [username], 'follow', json, (reerr, reresult) => {
                         if (reerr === null) {
                             saveResteemedLog(result[0].author, result[0].permlink)
+                            // This will show notification when using
                             console.log('Resteemed Successfull !!! of Author :' + result[0].author);
                         }
                         else {
+                            // This will show notification when using
                             console.log('Resteeme Failed !!! of Author :' + result[0].author);
                         }
                     });
@@ -48,12 +62,15 @@ function resteemPost() {
     }
 }
 
+// This function will dectact that is this post is applicable for resteem
 function IsValidPost(author, permlink) {
     if (rlinq.from(userlistforResteem).where(x => x == author).toArray().length == 0) {
+        // This will show notification when using
         console.log('Post of different author !!!');
         return false;
     }
     else if (rlinq.from(preresteemed).where(x => x.permlink == permlink).toArray().length > 0) {
+        // This will show notification when using
         console.log('Already Resteemed !!!');
         return false;
     }
@@ -62,6 +79,7 @@ function IsValidPost(author, permlink) {
     }
 }
 
+//This function will make a log for store resteemed post
 function saveResteemedLog(author, permlink) {
     var aresteem = {
         author: author,
@@ -77,8 +95,8 @@ function saveResteemedLog(author, permlink) {
     });
 }
 
+// This function will return current date time
 function CurrentDate() {
     var date = new Date()
     return date.toString();
 }
-
