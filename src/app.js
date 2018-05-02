21e095fb24f80863f05bfa48106c2b8b25d488a5
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-02 20:03:33 +0600

    Added new function on appjs
    Added new function to store error log of Resteem

--- src/app.js
+++ src/app.js
@@ -21,6 +21,17 @@ if (fs.existsSync('resteemed.json')) {
     }
 }
 
+// This array will stores Previous Error of Resteemed when application load
+var preErrorresteemed = [];
+// This will Retrive the Data of resteemed.json file
+if (fs.existsSync('resteemedError.json')) {
+    const resteemedError = JSON.parse(fs.readFileSync("resteemedError.json"));
+    if (resteemedError.length) {
+        // Storing error resteemed Data to preErrorresteemed which will use on application runtime
+        preErrorresteemed = resteemedError;
+    }
+}
+
 // This will show notification when using
 console.log('Application Initialized...');
 
@@ -47,11 +58,14 @@ function resteemPost() {
                     }]);
                     steem.broadcast.customJson(wif, [], [username], 'follow', json, (reerr, reresult) => {
                         if (reerr === null) {
+                            // calling this function to store resteem data
                             saveResteemedLog(result[0].author, result[0].permlink)
                             // This will show notification when using
                             console.log('Resteemed Successfull !!! of Author :' + result[0].author);
                         }
                         else {
+                            // calling this function to store error of resteem data
+                            saveResteemedErrorLog(result[0].author, result[0].permlink, reerr)
                             // This will show notification when using
                             console.log('Resteeme Failed !!! of Author :' + result[0].author);
                         }
@@ -86,9 +100,27 @@ function saveResteemedLog(author, permlink) {
         permlink: permlink,
         transactionTime: CurrentDate()
     };
-
+    // Adding new resteem info with previous
     preresteemed.push(aresteem);
     fs.writeFile('resteemed.json', JSON.stringify(preresteemed), function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+//This function will make a Error log for store Error info
+function saveResteemedErrorLog(author, permlink, error) {
+    var aresteem = {
+        author: author,
+        permlink: permlink,
+        error: error.message,
+        transactionTime: CurrentDate()
+    };
+
+    // Adding new resteem error info with previous
+    preErrorresteemed.push(aresteem);
+    fs.writeFile('resteemedError.json', JSON.stringify(preErrorresteemed), function (err) {
         if (err) {
             console.log(err);
         }
